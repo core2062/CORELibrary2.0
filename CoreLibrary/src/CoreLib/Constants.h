8e90ebd5d66d9115427a4984f1b8c4f3aa744a78
COREProgrammers <core2062programmers@googlegroups.com>
2015-09-22 17:49:55 -0500

    Base Code Branch Completion

--- CoreLibrary/src/CoreLib/Constants.h
+++ CoreLibrary/src/CoreLib/Constants.h
@@ -5,8 +5,11 @@
  *      Author: core
  */
 
+
 #ifndef SRC_CORELIB_CONSTANTS_H_
 #define SRC_CORELIB_CONSTANTS_H_
+
+#include <string>
 
 enum motors{
 
@@ -24,4 +27,9 @@ enum digitalSensors{
 enum analogSensors{
 
 };
+
+/* SMART DASHBOARD CONSTANTS */
+const std::string TESTVALUE = "Test_Value";
+
+
 #endif /* SRC_CONSTANTS_H_ */
